1c50cb8e8b8db3c4fdfd6bc1c17d5bc4be8b2224
tri <trin.saputra@gmail.com>
2022-05-16 14:15:52 +0700

    declare 4 basic math function

---
+++ javascript/main.js
@@ -0,0 +1,19 @@
+function addOperator (firstNumber, secondNumber) {
+	return firstNumber + secondNumber;
+};
+
+function subtractOperator (firstNumber, secondNumber) {
+	return firstNumber - secondNumber;
+};
+
+function multiplyOperator (firstNumber, secondNumber) {
+	return firstNumber * secondNumber;
+};
+
+
+function divideOperator (firstNumber, secondNumber) {
+	return firstNumber / secondNumber;
+};
+
+
+
